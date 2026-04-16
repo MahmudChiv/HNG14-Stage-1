4f296e471fab1791f3ae564d74e3c15112296b1f
Mahmud Adegboyega <mahmud.adegboyega@gmail.com>
2026-04-16 10:55:04 +0100

    Modified: server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,11 @@
 import express from "express";
-import dotenv from "dotenv";
-import { Country, RequestBody } from "./app.types";
-import axios from "axios";
 import cors from "cors";
-import { AppError } from "./App.error";
-import { User } from "./db.model";
 import appRoutes from "./app.routes";
 import { sequelize } from "./db.config";
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT! || 4000;
 
 app.use(express.json());
 app.use(cors());
@@ -32,6 +27,6 @@ app.get("/", (req, res) => {
   res.send("API is running 🚀");
 });
 
-app.listen(port, () =>
-  console.log(`Server running on http://localhost:${port}`),
+app.listen(process.env.PORT!, () =>
+  console.log(`Server running on http://localhost:${process.env.PORT!}`),
 );
